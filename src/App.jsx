149cc0a69687a4b7de4cdaddbdda2b6f0de9e114
Aden Cronje <adencronje05@gmail.com>
2024-08-06 15:58:06 +0200

    Created Vans component and fetched data from fake api data in server.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../assets/pages/Home";
 import About from "../assets/pages/About";
+import Vans from "../assets/pages/Vans";
 import "../server";
 
 function App() {
@@ -14,11 +15,15 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link className="" to="/vans">
+            Vans
+          </Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
