81ba6b2d478ed142ae6f1ccf5b3750a9ef729bf9
Aden Cronje <adencronje05@gmail.com>
2024-08-06 16:20:19 +0200

    Created link from van elements to vanDetails component

--- assets/pages/Vans.jsx
+++ assets/pages/Vans.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
@@ -9,18 +10,20 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  //   Styling of html page for vans
+  //   Styling of html page for vans + linking to van details
   const vanElements = vans.map((van) => (
     <div key={van.id} className="van-tile">
-      <img src={van.imageUrl} />
-      <div className="van-info">
-        <h3>{van.name}</h3>
-        <p>
-          ${van.price}
-          <span>/day</span>
-        </p>
-      </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
     </div>
   ));
 
